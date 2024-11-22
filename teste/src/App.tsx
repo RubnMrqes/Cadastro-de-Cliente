a86307ae64c8ac477bb02e09145567eedb2273c0
RubnMrqes <rubendobby16@gmail.com>
2024-11-22 01:41:11 -0300

    Alteração 1

--- teste/src/App.tsx
+++ teste/src/App.tsx
@@ -1,34 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   
+   <div className= "  h-screen w-full bg-slate-900 flex flex-col justify-center items-center">
+    <h1 className= "text-4xl text-white mb- m-10"> Cadastro de usuários</h1>
+    <input type="email" placeholder='digite seu email' className="border border-blue-100 shadow-nd mb-3 bg-blue-600/50 text-center w-[400px]  rounded-md"></input>
+    <input type="password" placeholder='digite sua senha' className="border border-blue-100 shadow-nd bg-blue-600/50 text-center w-[400px]  rounded-md"></input>
+    <button className = "border border-white bg-gray-300 w-[250px] rounded-full mt-5"> Entrar</button>
+   
+   
+   </div>
+
   )
 }
 
